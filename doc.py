530942b616ca9cadfd8e90fce1aee18bca1e8ae7
noob-aViral <guptaavi352@gmail.com>
2021-01-17 16:07:09 +0530

    REMOVE DISPREANCY OF DATATYPES

--- doc.py
+++ doc.py
@@ -1,15 +1,16 @@
 import mysql.connector as mysql
 from docx.api import Document
 import pprint
+
 table_names = ["Item_Table", "Customer_Table", "Salesman_Table", "Sales_Order_Table", "Customer_Table1", "Loan_Table", "Borrower_Table", "Account_Table", "Depositor_Table"]
 document = Document("/home/avi/Downloads/file.docx")
 
 # CREATING A DATABSE NAMED STORE
-database_name = "store"
+database_name = "store1"
 db = mysql.connect(
 	host = "localhost", 
 	user = "root", 
-	passwd = "your_password",
+	passwd = "Maninhell1#",
 	)
 cursor = db.cursor()
 try:
@@ -20,12 +21,16 @@ except:
 db.commit()
 ###############################################################
 
-# USING THIS LITTLE CODE TO DELETE ALL THE TABLES, IN CASE WE WANT TO RE-RUN THE CODE ON THE SAME DATABSE WITH THE SAME TABLES
+decimal = ["item_rate", "Sell_prize", "Bal_due"]
+integer = ["Pin", "Bal_due", "Salary", "Tot_quiantity_order", "Amount", "Balance"]
+
+sqlFile = open("sqlFile.txt",'w+')
+# USING THIS LITTLE CODE TO DELETE ALL THE TABLES, IN CASE WE WANT TO RE-RUN THE CODE ON THE SAME DATABASE WITH THE SAME TABLES
 for i in range(len(document.tables)):
 	db = mysql.connect(
 		host = "localhost", 
 		user = "root", 
-		passwd = "your_password",
+		passwd = "Maninhell1#",
 		database = database_name
 		)
 	cursor = db.cursor()
@@ -56,20 +61,27 @@ for i in range(len(document.tables)):
 	db = mysql.connect(
 		host = "localhost", 
 		user = "root", 
-		passwd = "your_password",
+		passwd = "Maninhell1#",
 		database = database_name
 		)
 
 	cursor = db.cursor()
 	create_query = "CREATE TABLE " + table_names[i] + " ("
-
+	sqlFile.write(str(i+1)+". " +table_names[i].upper()+'\n')
 	for j in range(len(keys)):
-		item = str(keys[j]).strip().replace(" ","").replace("\n","") + " VARCHAR(30) NOT NULL, "
+		item = str(keys[j]).strip().replace(" ","").replace("\n","")
+		if item in decimal:
+			item += " FLOAT(2) NOT NULL, "
+		elif item in integer:
+			item += " INT NOT NULL, "
+		else:
+			item +=" VARCHAR(30) NOT NULL, "
 		create_query += item
 		if(j == len(keys)-1):
 			create_query = create_query[:len(create_query)-2] + ");"
 
 	# print(create_query)
+	sqlFile.write(create_query + "\n")
 	cursor.execute(create_query)
 
 	insert_query = "INSERT INTO " + table_names[i] + " VALUES ("
@@ -77,12 +89,15 @@ for i in range(len(document.tables)):
 		row_data = data[j]
 		item = ""
 		for k in row_data:
-			item += "'" + str(k) + "', "
+			item += "'" + str(k).strip().replace(" ","").replace("\n","") + "', "
 		item = item[:-2] + ');'
 		# print(insert_query + item)
+		sqlFile.write(insert_query + item + "\n")
 		cursor.execute(insert_query + item)
 
 	db.commit()
 	###############################################################
-	print("Table " + str(i) + " complete.")
+	sqlFile.write("Table " + str(i+1) + " complete.\n\n")
+	print("Table " + str(i+1) + " complete.")
 
+sqlFile.close()
